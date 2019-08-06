fbe35aa9b167734450d02d3dec1688dbb7ef9970
unknown <coderingio@gmail.com>
2019-08-05 23:51:53 -0600

    Add form for area of room calculation, clean up files

--- ExerciseGui/ExerciseGui/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include,path
-from django.contrib import admin
-
-urlpatterns = [
-    path('exercises/', include('exercises.urls')),
-    path('admin/', admin.site.urls),
-]
